9eb7d3fe17056b6504fca8c8bc7e9991af4e3a04
Maicon <maiconwilli91@gmail.com>
2024-06-28 04:41:11 -0300

    enviando arquivos para o envio diário de relatório por e-mail

---
+++ app/Console/Commands/SendDailySalesReport.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Venda;
+use App\Mail\DailySalesReport;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Carbon;
+
+class SendDailySalesReport extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sales:send-report';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Enviar relatório diário via e-mail';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $data_inicial = Carbon::now()->startOfDay();
+        $data_final = Carbon::now()->endOfDay();
+        $vendas_do_dia = Venda::whereBetween('created_at',[$data_inicial, $data_final])->get();
+
+        $totalVendas = $vendas_do_dia->sum('valor_total');
+
+        Mail::to('teste@gmail.com')->send(new DailySalesReport($vendas_do_dia, $totalVendas));
+
+      Log::info('O comando sales:send-report está sendo executado.');
+    
+
+    }
+}
